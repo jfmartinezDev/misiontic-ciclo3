2aacd59017f4aa0f8fd210b7da3a65c974e4cca4
jodatm <joshuadtm@gmail.com>
2021-09-07 09:47:13 -0500

    grupo20

---
+++ grupo20/js/ejemplo2.js
@@ -0,0 +1,17 @@
+/*let contenido = "Misiontic 2022";
+const contenido2 = "Grupo 20";
+
+document.getElementById("parrafo").innerHTML = contenido;*/
+
+/*let variable = "misiontic";
+if (variable.endsWith("mi")){
+    console.log("entra");
+}*/
+
+//let variable = "a";
+
+//console.log(variable.repeat(7));
+
+//console.log(`${variable}->${variable.repeat(7)}` );
+
+document.getElementById("parrafo").innerHTML = new Date().getMinutes();
